0745f97b92161eeb4e1ffef354c5b82bd9f931d3
ArielFol <folgueiraariel@gmail.com>
2026-03-22 21:31:43 -0300

    Ej3: implementacion inicial

---
+++ validar-echo-server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+RED="testing_net"
+SERVER="server"
+PORT="12345"
+MENSAJE="prueba"
+
+RESPUESTA=$(docker run --rm --net $RED alpine sh -c "
+            echo -n '$MENSAJE' | nc $SERVER $PORT
+            ")
+if [ "$RESPUESTA" == "$MENSAJE" ]; then
+    echo "action: test_echo_server | result: success"
+else
+    echo "action: test_echo_server | result: fail"
+fi
