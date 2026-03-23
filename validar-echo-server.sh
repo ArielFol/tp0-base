5e01535816d0161cec6408c29934eef8e0a0d76d
ArielFol <folgueiraariel@gmail.com>
2026-03-22 21:50:31 -0300

    Ej3: fix: se corrigio el ==

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -8,7 +8,7 @@ MENSAJE="prueba"
 RESPUESTA=$(docker run --rm --net $RED alpine sh -c "
             echo -n '$MENSAJE' | nc $SERVER $PORT
             ")
-if [ "$RESPUESTA" == "$MENSAJE" ]; then
+if [ "$RESPUESTA" = "$MENSAJE" ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
